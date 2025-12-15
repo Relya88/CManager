904614fce44d34d9dfba0aa688701c81e499a5e3
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2025-12-15 13:58:22 +0100

    Recreated Infrastructure project with with JSON repository, CustomerRepo and ICustomerRepo

---
+++ CManager.Infrastructure/Repos/ICustomerRepo.cs
@@ -0,0 +1,13 @@
+﻿using CManager.Domain.Models;
+using System.Collections.Generic;
+
+namespace CManager.Infrastructure.Repos;
+
+//Interfacet som definierar vilka metoder som kundrepot måste ha med ansvar för att spara och hämta kunder från jsonfil
+//listn hämtar kunder från jsonfil och returnerar dem som lista och boolen returnerar om sparnigenn lyckades
+public interface ICustomerRepo
+{
+    List<CustomerModel> GetAllCustomers();
+
+    bool SaveCustomers(List<CustomerModel> customers);
+}
