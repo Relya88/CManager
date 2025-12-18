8ff87fb1424a2d9e5e5b420b7d9d0faeb7b16ad7
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2025-12-18 18:42:52 +0100

    Domain: Added customer and adress models

---
+++ CManager.Core.Domain/Models/AddressModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CManager.Core.Domain.Models;
+
+// Ren datamodell som innehåller endast information om adressen och ingen logik alls (som Emil visade).
+public class AddressModel
+{
+    public string Street { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
+
---
+++ CManager.Core.Domain/Models/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CManager.Core.Domain.Models;
+
+//Står för en kund i systemet och klassen används i alla lager men utan logik
+
+public class CustomerModel
+{
+    // guid för kundens unika id
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+
+    //för bättre struktur skrevs addresssen som en separat modell 
+    public AddressModel Address { get; set; } = null!;
+}
+
